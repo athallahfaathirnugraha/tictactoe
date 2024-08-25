9719319467def68576d5d033dcab7fad2cb8b9f1
athallahfaathirnugraha <athallahfaathirnugraha@gmail.com>
2024-08-25 12:44:33 +0700

    use svg

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const tds = document.getElementsByTagName("td");
 
 for (const td of tds) {
-  td.addEventListener("click", (e) => e.currentTarget.innerHTML = "x");
+  td.addEventListener("click", onClick);
+}
+
+function onClick(e) {
+  addX(e.currentTarget.children[0]);
+}
+
+function addX(svg) {
+  const xSvg = "<line x1=\"0\" y1=\"0\" x2=\"100%\" y2=\"100%\" /><line x1=\"100%\" y1=\"0\" x2=\"0\" y2=\"100%\" />";
+  svg.innerHTML = xSvg;
 }
