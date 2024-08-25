e06f49dd2d0e593358cfc1c0d14771ff49ef75a9
athallahfaathirnugraha <athallahfaathirnugraha@gmail.com>
2024-08-25 18:16:39 +0700

    win detection

--- script.js
+++ script.js
@@ -31,6 +31,13 @@ function onClick(e) {
       turn = "x";
       break;
   }
+
+  const win = checkWin(board);
+  console.log(win);
+
+  if (win !== null) {
+    alert(`${win} won`);
+  }
 }
 
 function addX(svg) {
@@ -41,4 +48,30 @@ function addX(svg) {
 function addCircle(svg) {
   const circleSvg = "<circle cx=\"50%\" cy=\"50%\" r=\"50%\" fill=\"none\" />"
   svg.innerHTML = circleSvg;
+}
+
+function checkWin(board) {
+  // horizontal
+  for (let i = 0; i < 3; i++) {
+    if (board[i][0] === "" || board[i][1] === "" || board[i][2] === "") break;
+    if (board[i][0] === board[i][1] && board[i][1] === board[i][2]) return board[i][0];
+  }
+
+  // vertical
+  for (let i = 0; i < 3; i++) {
+    if (board[0][i] === "" || board[1][i] === "" || board[2][i] === "") break;
+    if (board[0][i] === board[1][i] && board[1][i] === board[2][i]) return board[0][i];
+  }
+
+  // diagonal
+  if (board[0][0] === "" || board[1][1] === "" || board[2][2] === "" || board[0][2] === "" || board[2][0] === "") return null;
+
+  if (
+    board[0][0] === board[1][1] && board[1][1] === board[2][2] ||
+    board[0][2] === board[1][1] && board[1][1] === board[2][0]
+  ) {
+    return board[0][0];
+  }
+
+  return null;
 }
